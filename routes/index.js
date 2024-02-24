bb5102f9ea0b93460bc4eaa7b64238196f9490d3
sharky3102 <petarsarec2006@gmail.com>
2024-02-24 22:01:23 +0100

    push

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+
+// GET /
+router.get("/", function (req, res, next) {
+  res.render("index");
+});
+
+// GET /
+router.get("/naslovnica", function (req, res, next) {
+  res.render("naslovnica");
+});
+module.exports = router;
